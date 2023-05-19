166c310c17ba484e44f5827f54863d1242944c32
ivarvani <i.varvani@gmail.com>
2023-05-20 08:55:26 +1000

    rectified the doubling function

--- doubling.py
+++ doubling.py
@@ -1,3 +1,4 @@
-value= print(input("please enter a number not equal to 0"))
-answer = value * 3
-print (answer)
+# doubling 
+var1= print(int(input("please enter a number not equal to 0     ")))
+print((var1) * 2)
+
