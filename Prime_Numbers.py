78bf943451e5697b5d1dba96f19a617c6c3e6424
ivarvani <i.varvani@gmail.com>
2023-05-24 19:59:58 +1000

    deeveloped times table function using while and for loop and modified new function

--- Prime_Numbers.py
+++ Prime_Numbers.py
@@ -6,5 +6,6 @@ for n in range(2, c + 1):
     if c % n >= 1:
         continue
     if c % n == 0 and c != n:
+        print("c ic not a prime number")
         break
     print(f"{c} is a prime number")
