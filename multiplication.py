fc282d8e6ebd3a6a968e475840789d39deb57d4f
ivarvani <i.varvani@gmail.com>
2023-06-10 16:22:42 +1000

    adding and commiting

--- multiplication.py
+++ multiplication.py
@@ -4,7 +4,7 @@ if Z == 0:
     print("you have entered a zero")
 else:
     for x in range (1,11):
-        print (Z , "*" , x , "=", (x * Z))
+        print (f"{Z}*{x} = {x * Z}")
 print("thank you")
 
 
@@ -13,7 +13,7 @@ X = int(input("Please input a value for the times table"))
 Y= 1
 while Y <= 15:
     if X>=1:
-        print(X, "*" , Y , "=", (X*Y))
+        print(f"{X}*{Y}={X*Y}")
         Y+=1
     else:
         print("you have entered an invalid number")
